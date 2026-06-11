e8d3ed68a93e48a17728d7a250d4b2f69b3d04ed
hanakhalda11-del <hanakhalda11@gmail.com>
2026-06-11 18:46:35 +0700

    Implement preorder traversal in BinaryTree
    Added preorder traversal method to BinaryTree class.

--- searchtree.cpp
+++ searchtree.cpp
@@ -93,4 +93,20 @@ public :
         }
      }
 
+     void preorder (Node *ptr)
+     {
+        if (ROOT == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            cout << ptr -> info << " ";
+            preorder (ptr -> leftchild);
+            preorder (ptr -> rightchild);
+        }
+     }
+
      
