94985ac30d10da9b6f6665a2bbc02924bf702781
hanakhalda11-del <hanakhalda11@gmail.com>
2026-06-11 18:40:22 +0700

    restore BinaryTree class structure

--- searchtree.cpp
+++ searchtree.cpp
@@ -40,4 +40,12 @@ public :
         Node *parent = NULL;
         Node *currentNode = NULL;
 
+        search (element, parent, currentNode);
+
+        if (parent == NULL)
+        {
+            ROOT = newNode;
+            return;
+        }
+        
         
