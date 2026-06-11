4bf4589cc87e9931088d46411464106ca4fa5f31
hanakhalda11-del <hanakhalda11@gmail.com>
2026-06-11 18:46:04 +0700

    Implement inorder traversal for BinaryTree
    Added inorder traversal function to BinaryTree class.

--- searchtree.cpp
+++ searchtree.cpp
@@ -77,4 +77,20 @@ public :
         }
      }
 
+     void inorder (Node *ptr)
+     {
+        if (ROOT == NULL)
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+
+        if (ptr != NULL)
+        {
+            inorder (ptr -> leftchild);
+            cout << ptr -> info << " ";
+            inorder (ptr -> rightchild);
+        }
+     }
+
      
